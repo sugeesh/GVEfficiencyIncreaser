27d187592d003a8a6dc887b502c819faa1e2e9aa
buddhiv <92buddhiv@gmail.com>
2015-12-10 22:18:10 +0530

    New file added

---
+++ request_certificate.php
@@ -0,0 +1,27 @@
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link type="text/css" rel="stylesheet" href="bootstrap-3.3.6-dist/css/bootstrap.css">
+    <title>Attorney General</title>
+</head>
+<body>
+<p><img src="http://www.brandsoftheworld.com/sites/default/files/styles/logo-thumbnail/public/042011/government_logo_of_sri_lanka-converted.png?itok=er9QPW5U" alt="headImg" width="152" height="129" /><a href="https://www.google.lk/url?sa=t&amp;rct=j&amp;q=&amp;esrc=s&amp;source=web&amp;cd=1&amp;sqi=2&amp;ved=0ahUKEwiQ8Kv79s3JAhVBthoKHX6lDq4QFggbMAA&amp;url=http%3A%2F%2Fwww.attorneygeneral.gov.lk%2F&amp;usg=AFQjCNGi6TFu7bmnNUcgrj8PAhr2JHl6RA&amp;bvm=bv.109332125,d.d2s&amp;cad=rja" onmousedown="return rwt(this, '', '', '', '1', 'AFQjCNGi6TFu7bmnNUcgrj8PAhr2JHl6RA', '', '0ahUKEwiQ8Kv79s3JAhVBthoKHX6lDq4QFggbMAA', '', '', event)">Attorney General's Department - Sri Lanka</a>
+</p>
+<p>&nbsp;</p>
+<table width="1216" border="1" class="table table-bordered">
+    <tr>
+        <th width="284" scope="row">Receive Date</th>
+        <td width="284">Application_Id</td>
+        <td width="284">Status</td>
+        <td width="284">&nbsp;</td>
+    </tr>
+    <form id="form1" name="form1" method="post" action="index.php">
+        <?php echo $dynamicList; ?>
+    </form>
+
+</table>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+</body>
+
+</html>
