f7de42e8cc880a4ad9cfde01a6489505f52a0bdb
sugeesh <lpdneo@gmail.com>
2015-12-11 13:16:08 +0530

    Added View Application and not completed yet

--- index.php
+++ index.php
@@ -1,22 +1,31 @@
+
+
 <?php
 include "./connect_to_mysql.php";
 // Parse the form data andd inventory Item to the List
-if (isset($_POST['thisId'])) {
+if (isset($_POST['updateForm'])) {
     $thisId = mysql_real_escape_string($_POST['thisId']);
-    $now_status = mysql_real_escape_string($_POST['statusList'.$_POST['thisId']]);
+    $now_status = mysql_real_escape_string($_POST['statusList' . $_POST['thisId']]);
 
-    echo $now_status;
-    echo "ABCDS";
-    echo $thisId;
-    $results = mysql_query("UPDATE birthcertificateapplication SET status=$now_status  WHERE application_id= $thidId;");
+    echo "UPDATE birthcertificateapplication SET status=$now_status  WHERE application_id= $thisId;";
+    
+    $results = mysql_query("UPDATE birthcertificateapplication SET status='$now_status'  WHERE application_id= $thisId;");
+    header("location: index.php");
     exit();
 }
 ?>
 
 
 <?php
+
+//states
+//received = 0
+//searching = 1
+//found = 2
+//posted = 3
+
 $dynamicList = "";
-$result = mysql_query("SELECT * FROM birthcertificateapplication WHERE ag_office_id=1");
+$result = mysql_query("SELECT * FROM birthcertificateapplication WHERE ag_office_id=1 Order by status");
 
 if (mysql_num_rows($result) > 0) {
     while ($row = mysql_fetch_array($result)) {
@@ -25,41 +34,52 @@ if (mysql_num_rows($result) > 0) {
         $status = $row["status"];
         $date_added = strftime("%b %d, %Y", strtotime($row["Date"]));
         $tableColor = "";
+        
+        //select 4
         $receivedSelect = "";
         $searchingSelect = "";
         $foundSelect = "";
         $postedSelect = "";
-        if ($status == 'Received') {
+        
+        //disable posted mails
+        $postedDisable = "";
+        
+        
+        if ($status == 0) {
             $tableColor = "danger";
             $receivedSelect = "selected";
-        } elseif ($status == 'searching') {
+        } elseif ($status == 1) {
             $tableColor = "warning";
             $searchingSelect = "selected";
-        } elseif ($status == 'found') {
+        } elseif ($status == 2) {
             $tableColor = "info";
             $foundSelect = "selected";
-        } elseif ($status == 'posted') {
+        } elseif ($status == 3) {
             $tableColor = "success";
             $postedSelect = "selected";
+            $postedDisable = "disabled";
         }
-        $dynamicList .= '<tr class=' . $tableColor . '>
+        $dynamicList .= ' <form id = "form'.$application_id.'" name = "form'.$application_id.'" method = "post" action = "index.php">'
+                . '<tr class=' . $tableColor . '>
                 <th scope="row">' . $date_added . '</th>
-                <td>'.$application_id.'</td>
+                <td> ' . $application_id . ' <a href="www.google.com " >View Form </a></p></td>
                 <td>
-                        <select name="statusList'.$application_id.'" id="statusList" class="form-control" >
-                            <option ' . $receivedSelect . ' value="received">Received</option>
-                            <option ' . $searchingSelect . '  value="searching">Still Searching</option>
-                            <option ' . $foundSelect . ' value="found">Found</option>
-                            <option ' . $postedSelect . ' value="mail">Posted Through Mail</option>
+                        <select name="statusList' . $application_id . '" id="statusList" class="form-control" >
+                            <option ' . $receivedSelect . ' value=0>Received</option>
+                            <option ' . $searchingSelect . '  value=1>Still Searching</option>
+                            <option ' . $foundSelect . ' value=2>Found</option>
+                            <option ' . $postedSelect . ' value=3>Posted Through Mail</option>
                         </select>
-                        <input name="thisId'. $application_id .'" type="hidden" value=' . $application_id . ' >
+                        <input name="thisId" type="hidden" value=' . $application_id . ' >
+                        <input name="updateForm" type="hidden" value='.$application_id.' >
                    
                 </td>
                 <td>
                 
-                <button type="submit" class="btn btn-success btn-block">Confirm</button></a>
+                <button type="submit" class="btn btn-success btn-block" '.$postedDisable.' >Confirm</button></a>
                 </td>
-            </tr>';
+            </tr>
+            </form>';
     }
 } else {
     die("something failed");
@@ -82,19 +102,24 @@ and open the template in the editor.
         <title>Attorney General</title>
     </head>
     <body>
-        <p><img src="http://www.brandsoftheworld.com/sites/default/files/styles/logo-thumbnail/public/042011/government_logo_of_sri_lanka-converted.png?itok=er9QPW5U" alt="headImg" width="152" height="129" /><a href="https://www.google.lk/url?sa=t&amp;rct=j&amp;q=&amp;esrc=s&amp;source=web&amp;cd=1&amp;sqi=2&amp;ved=0ahUKEwiQ8Kv79s3JAhVBthoKHX6lDq4QFggbMAA&amp;url=http%3A%2F%2Fwww.attorneygeneral.gov.lk%2F&amp;usg=AFQjCNGi6TFu7bmnNUcgrj8PAhr2JHl6RA&amp;bvm=bv.109332125,d.d2s&amp;cad=rja" onmousedown="return rwt(this, '', '', '', '1', 'AFQjCNGi6TFu7bmnNUcgrj8PAhr2JHl6RA', '', '0ahUKEwiQ8Kv79s3JAhVBthoKHX6lDq4QFggbMAA', '', '', event)">Attorney General's Department - Sri Lanka</a>
+        <p>
+            
+            <a href="https://www.google.lk/url?sa=t&amp;rct=j&amp;q=&amp;esrc=s&amp;source=web&amp;cd=1&amp;sqi=2&amp;ved=0ahUKEwiQ8Kv79s3JAhVBthoKHX6lDq4QFggbMAA&amp;url=http%3A%2F%2Fwww.attorneygeneral.gov.lk%2F&amp;usg=AFQjCNGi6TFu7bmnNUcgrj8PAhr2JHl6RA&amp;bvm=bv.109332125,d.d2s&amp;cad=rja" onmousedown="return rwt(this, '', '', '', '1', 'AFQjCNGi6TFu7bmnNUcgrj8PAhr2JHl6RA', '', '0ahUKEwiQ8Kv79s3JAhVBthoKHX6lDq4QFggbMAA', '', '', event)">Sri Lanka</a>
         </p>
-        <p>&nbsp;</p>
-        <table width="1216" border="1" class="table table-bordered">
+        <p>&nbsp;
+        </p>
+        <table width = "1216" border = "1" class = "table table-bordered">
             <tr>
-                <th width="284" scope="row">Receive Date</th>
-                <td width="284">Application_Id</td>
-                <td width="284">Status</td>
-                <td width="284">&nbsp;</td>
+                <th width = "284" scope = "row">Receive Date</th>
+                <td width = "284">Application_Id</td>
+                <td width = "284">Status</td>
+                <td width = "284">&nbsp;
+                </td>
             </tr>
-            <form id="form1" name="form1" method="post" action="index.php">
-            <?php echo $dynamicList; ?>
-            </form>
+<!--            <form id = "form1" name = "form1" method = "post" action = "index.php">-->
+                <?php echo $dynamicList;
+                ?>
+<!--            </form>-->
 
         </table>
         <p>&nbsp;</p>
